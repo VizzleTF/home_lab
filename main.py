71fb037af01bcb171c7d98922194f832c02d5f98
Ivan Kvashonkin <iskvashonkin@activebc.ru>
2024-08-03 21:13:43 +0300

    delete 10 files and update 5 files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import proxmox, keenetic
+import proxmox
 
 app = Flask(__name__)
 
@@ -9,50 +9,22 @@ def home():
 
 @app.route('/containers.html', methods=['GET', 'POST'])
 def containers():
-    if proxmox.vmstatus(103) == 'running':
-        adguard = True
-    else:
-        adguard = False
-
-    if proxmox.vmstatus(101) == 'running':
-        plex = True
-    else:
-        plex = False
-
-    if proxmox.vmstatus(100) == 'running':
-        nextcloud = True
-    else:
-        nextcloud = False
-
-    if proxmox.vmstatus(104) == 'running':
-        nginx = True
-    else:
-        nginx = False
+    vms = proxmox.get_all_vms()
 
     if request.method == 'POST':
-        if request.form.get('action1') == 'AdGuard':
-            proxmox.vmreset(103)
-        elif request.form.get('action1') == 'Plex':
-            proxmox.vmreset(101)
-        elif request.form.get('action1') == 'Nextcloud':
-            proxmox.vmreset(100)
-        elif request.form.get('action3') == 'AdGuard':
-            proxmox.vmstart(103)
-        elif request.form.get('action1') == 'Proxy':
-            proxmox.vmreset(104)
-        elif request.form.get('action3') == 'Proxy':
-            proxmox.vmstart(104)
-        elif request.form.get('action3') == 'Plex':
-            proxmox.vmstart(101)
-        elif request.form.get('action3') == 'Nextcloud':
-            proxmox.vmstart(100)
-        elif request.form.get('action2') == 'Провайдера':
-            keenetic.piholeoff()
-        elif request.form.get('action2') == 'Pihole':
-            keenetic.piholeon()
+        action = request.form.get('action')
+        vmid = request.form.get('vmid')
+        node = request.form.get('node')
 
+        if action == 'start':
+            proxmox.vmstart(node, vmid)
+        elif action == 'reset':
+            proxmox.vmreset(node, vmid)
 
-    return render_template('containers.html', nginx=nginx, adguard=adguard, plex=plex, nextcloud=nextcloud)
+        # Refresh VM status after action
+        vms = proxmox.get_all_vms()
+
+    return render_template('containers.html', vms=vms)
 
 if __name__ == '__main__':
     app.run(port='80')
--- proxmox.py
+++ proxmox.py
@@ -1,45 +1,34 @@
 from proxmoxer import ProxmoxAPI
 import urllib3
 
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def vmstatus(id):
-    try:
-        proxmox = ProxmoxAPI(
-            "10.11.12.13", user="root@pam", password="300124", verify_ssl=False
-        )
-        for vm in proxmox.nodes("home").qemu.get():
-            if vm['vmid'] == id :
-                return vm['status']
-    except Exception:
-        print('error')
+proxmox = ProxmoxAPI("10.11.12.13", user="root@pam", password="", verify_ssl=False)
 
-def vmstop(id):
-    try:
-        proxmox = ProxmoxAPI(
-            "10.11.12.13", user="root@pam", password="300124", verify_ssl=False
-        )
-        proxmox.nodes("home").qemu(id).status.post('stop')
-    except Exception:
-        print('error')
+def get_all_vms():
+    vms = []
+    for node in proxmox.nodes.get():
+        node_name = node['node']
+        for vm in proxmox.nodes(node_name).qemu.get():
+            vms.append({
+                'node': node_name,
+                'vmid': vm['vmid'],
+                'name': vm['name'],
+                'status': vm['status']
+            })
+    return vms
 
-def vmstart(id):
+def vm_action(node, vmid, action):
     try:
-        proxmox = ProxmoxAPI(
-            "10.11.12.13", user="root@pam", password="300124", verify_ssl=False
-        )
-        proxmox.nodes("home").qemu(id).status.post('start')
-    except Exception:
-        print('error')
+        proxmox.nodes(node).qemu(vmid).status.post(action)
+    except Exception as e:
+        print(f"Error performing {action} on VM {vmid} on node {node}: {str(e)}")
 
-def vmreset(id):
-    try:
-        proxmox = ProxmoxAPI(
-            "10.11.12.13", user="root@pam", password="300124", verify_ssl=False
-        )
-        proxmox.nodes("home").qemu(id).status.post('reset')
-    except Exception:
-        print('error')
+def vmstart(node, vmid):
+    vm_action(node, vmid, 'start')
 
-print(vmstatus(103))
+def vmstop(node, vmid):
+    vm_action(node, vmid, 'stop')
+
+def vmreset(node, vmid):
+    vm_action(node, vmid, 'reset')
